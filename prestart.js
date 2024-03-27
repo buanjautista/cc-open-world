95a8e1b5a594218ff7ce54803161c3eba986d0d1
Juanba <bauti777@gmail.com>
2024-03-27 14:23:04 -0300

    add: new rando tips, scripts separed, henry change

---
+++ prestart.js
@@ -0,0 +1,3 @@
+import './src/gui-workaround.js'
+import './src/custom-steps.js'
+import './src/patch-handling.js'
---
+++ src/gui-workaround.js
@@ -0,0 +1,18 @@
+export function defineGUIPlease() {
+  ig.lang.labels.sc.gui.menu.randomizer.sets["open-world"] = "Open World";
+  ig.lang.labels.sc.gui.menu.randomizer.options.descriptions["vt-skip"] = "Enables all floors of Vermillion Tower after the first fight";
+  ig.lang.labels.sc.gui.menu.randomizer.options.descriptions["meteor-vw"] = "Locks Vermillion Wasteland behind a Meteor Shade barrier";
+  ig.lang.labels.sc.gui.menu.randomizer.options.descriptions["open-fajro"] = "Opens up some locked doors in Upper Faj'ro Temple";
+  ig.lang.labels.sc.gui.menu.randomizer.options.names["vt-skip"] = "Skip Tower floors";
+  ig.lang.labels.sc.gui.menu.randomizer.options.names["meteor-vw"] = "Locked Wasteland";
+  ig.lang.labels.sc.gui.menu.randomizer.options.names["open-fajro"] = "Open Fajro";
+  ig.lang.labels.sc.gui.menu.randomizer.sets["open-world-sblock"] = "Open World - Vermillion Tower entry barrier";
+  ig.lang.labels.sc.gui.menu.randomizer.options.names["sblock-all"] = "Bosses and Shades";
+  ig.lang.labels.sc.gui.menu.randomizer.options.descriptions["sblock-all"] = "Block Vermillion Tower behind four element dungeon bosses and shades";
+  ig.lang.labels.sc.gui.menu.randomizer.options.names["sblock-boss"] = "Bosses only";
+  ig.lang.labels.sc.gui.menu.randomizer.options.descriptions["sblock-boss"] = "Block Vermillion Tower behind the four element dungeon bosses";
+  ig.lang.labels.sc.gui.menu.randomizer.options.names["sblock-shades"] = "Shades only";
+  ig.lang.labels.sc.gui.menu.randomizer.options.descriptions["sblock-shades"] = "Block Vermillion Tower behind the four element shades";
+  ig.lang.labels.sc.gui.menu.randomizer.options.names["sblock-none"] = "Disable VT barrier";
+  ig.lang.labels.sc.gui.menu.randomizer.options.descriptions["sblock-none"] = "Vermillion Tower is accessible at any time";
+}
